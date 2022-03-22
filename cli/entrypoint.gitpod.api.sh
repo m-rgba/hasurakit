c65c25f640b88629030b36939c7f1004424c6922
Martin Mark <martin@martinmark.com>
2022-03-22 04:31:26 +0000

    Gitpod POC

---
+++ cli/entrypoint.gitpod.api.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /usr/src/hasura
+
+echo "Starting Console API 💻 (https://9693-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST})"
+hasura console --log-level DEBUG --address "kit_cli_api" --endpoint "https://8080-${GITPOD_WORKSPACE_ID}.${GITPOD_WORKSPACE_CLUSTER_HOST}" --admin-secret "${HASURA_GRAPHQL_ADMIN_SECRET}" --no-browser || exit 1
