1fd804b03efd7c4db5828c53ea65137802ce9988
Martin Mark <martin@martinmark.com>
2022-04-03 21:54:35 -0400

    Format / Port Fix / docker-compose depends_on

--- recipes/cli-deploy/migrate.sh
+++ recipes/cli-deploy/migrate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Run migration.
 echo "Applying metadata... 📄"
 hasura metadata apply --endpoint "${HASURA_PROTOCOL}${HASURA_URL}" --admin-secret "${HASURA_GRAPHQL_ADMIN_SECRET}" || exit 1
 echo "Applying migrations... 💽"
@@ -7,3 +8,7 @@ hasura migrate apply --endpoint "${HASURA_PROTOCOL}${HASURA_URL}" --admin-secret
 echo "Reloading metadata... 📄"
 hasura metadata reload --endpoint "${HASURA_PROTOCOL}${HASURA_URL}" --admin-secret "${HASURA_GRAPHQL_ADMIN_SECRET}" || exit 1
 echo "Migration complete ✅"
+
+# (Optional) Running in case any follow-up actions.
+#            If removed - will finish with status 0 (success) if successful.
+tail -f /dev/null
